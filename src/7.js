1627ec2546dc66eaf30247e165043614c4f8aca0
fruehfanizzi837 <fruehfanizzi837@gmail.com>
2025-03-12 00:46:46 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>School Project</title>
+</head>
+<body>
+    <h1>Welcome to my school project!</h1>
+    <p>This is a simple school project that I am working on.</p>
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+</body>
+</html>
