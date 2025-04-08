2fa993c103bc7cacb3a089754655b16e136820f9
fruehfanizzi837 <fruehfanizzi837@gmail.com>
2025-04-08 05:28:29 +0000

    Add 28.js

---
+++ src/28.js
@@ -0,0 +1,10 @@
+// Example of a simple school project using JavaScript
+
+// Function to perform some calculations or tasks
+function calculateSum(numbers) {
+  let sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return sum;
+}
+
+// Main function to run the calculation
+calculateSum([1, 2, 3, 4, 5]);
